d364489d85f16b81d18dee480162dfb34bc506fd
gayanieka <gayaniekanayake00@gmail.com>
2025-09-21 17:00:16 +0530

    changes in index-gayani

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.use("/api/deliveries", deliveryRoutes);
 app.use("/api/couriers", courierRoutes);
 app.use("/api/invoices",invoiceRoute);
 app.use("/api/dashboard", dashboardRoutes);
-app.use("/api/users", userRoutes);
+
 
 
 
